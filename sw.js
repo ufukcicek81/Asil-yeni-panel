0fa91ebd5ffdd1761954fab824a2b1c8408478ea
ufukcicek81 <112567082+ufukcicek81@users.noreply.github.com>
2026-03-19 00:27:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = 'asil-kuyumcum-v1';
-const urlsToCache = [
-  '/altin/',
-  '/altin/index.html'
-];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE_NAME; })
-            .map(function(k) { return caches.delete(k); })
-      );
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  // cache zorlamıyoruz, sadece PWA için boş fetch handler
 });
